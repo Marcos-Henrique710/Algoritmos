b97bcb7e4459897dbc2cb0005645fd2ae0219c43
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-06-17 10:12:55 -0300

    Q1_L5.cpp

---
+++ Listas de algoritmos/lista 5/Q1_L5.cpp
@@ -0,0 +1,36 @@
+/*
+1. Elabore um algoritmo que leia uma matriz M(6,6) e um valor A inteiro e multiplique a matriz M pelo valor A
+inserindo os resultados em um vetor de V(36). Ao final, escreva o vetor V. 
+*/
+#include <iostream> 
+using namespace std;
+int main(){
+//Criação das variáveis:
+int M[6][6], A, vetorA[36], i, j;
+  
+//Entrada e saída de dados:
+cout << "\n\tDigite os itens da matriz (6x6): ";
+for(i = 0; i < 6; i++){
+    for(i = 0; i < 6; i++){
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
