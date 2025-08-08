a112b129615de69b3e95761ad44396cc0871450b
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 20:20:51 -0300

    Update L5_Q10.cpp

--- Listas de algoritmos/lista 5/L5_Q10.cpp
+++ Listas de algoritmos/lista 5/L5_Q10.cpp
@@ -5,9 +5,15 @@ void q10(){
     const int N=12;
     double A[N][N];
     cout<<"Q10: leia 144 numeros reais (12x12):\n";
-    for(int i=0;i<N;i++) for(int j=0;j<N;j++) cin>>A[i][j];
-    double sum=0; int cnt=0;
-    for(int i=1;i<N;i++) for(int j=0;j<i;j++){ sum+=A[i][j]; cnt++; }
+    
+    for(int i=0;i<N;i++) 
+        for(int j=0;j<N;j++) cin>>A[i][j];
+            double sum=0; 
+            int cnt=0;
+                for(int i=1;i<N;i++) 
+                    for(int j=0;j<i;j++){ 
+                        sum+=A[i][j]; cnt++;
+                    }
     cout<<"Media abaixo da diagonal principal: "<<(cnt?sum/cnt:0)<<"\n";
 }
 
