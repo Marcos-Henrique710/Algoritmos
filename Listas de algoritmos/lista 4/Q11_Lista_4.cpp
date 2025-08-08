ae1c5ac335dacb7bac18827fbba6dc62e6bf8f51
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 20:56:13 -0300

    Update Q11_Lista_4.cpp

--- Listas de algoritmos/lista 4/Q11_Lista_4.cpp
+++ Listas de algoritmos/lista 4/Q11_Lista_4.cpp
@@ -14,10 +14,16 @@ using namespace std;
 
 int main(){
     vector<int> v(20);
-    for(int i=0;i<20;i++) cin>>v[i];
-    int maxdiff = abs(v[1]-v[0]); int idx=0;
+    for(int i=0;i<20;i++) 
+        cin>>v[i];
+    int maxdiff = abs(v[1]-v[0]); 
+    int idx=0;
     for(int i=1;i<20;i++){
-        int d = abs(v[i]-v[i-1]); if(d>maxdiff){ maxdiff=d; idx=i-1; }
+        int d = abs(v[i]-v[i-1]); 
+        if(d>maxdiff){ 
+            maxdiff=d; 
+            idx=i-1;
+        }
     }
     cout<<maxdiff<<" "<<idx<<" "<<idx+1<<"\n";
     return 0;
