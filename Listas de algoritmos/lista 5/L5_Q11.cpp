f7611aec482a51a5c0798900b6c65b6e97a461a3
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:41:48 -0300

    Update L5_Q11.cpp

--- Listas de algoritmos/lista 5/L5_Q11.cpp
+++ Listas de algoritmos/lista 5/L5_Q11.cpp
@@ -2,14 +2,26 @@
 11. Leia matriz MxN verifique e escreva coordenadas do maior e do menor elementos
 */
 void q11(){
-    int M,N; cout<<"Q11: linhas m e colunas n: "; cin>>M>>N;
+    int M,N; 
+    cout<<"\n\tDigite linhas m e colunas n: "; 
+    cin>>M>>N;
     vector<vector<int>> A(M, vector<int>(N));
-    for(int i=0;i<M;i++) for(int j=0;j<N;j++) cin>>A[i][j];
+    for(int i=0;i<M;i++) 
+        for(int j=0;j<N;j++) 
+            cin>>A[i][j];
     int mx=A[0][0], mn=A[0][0], mxr=0,mxc=0,mnr=0,mnc=0;
-    for(int i=0;i<M;i++) for(int j=0;j<N;j++){
-        if(A[i][j]>mx){ mx=A[i][j]; mxr=i; mxc=j; }
-        if(A[i][j]<mn){ mn=A[i][j]; mnr=i; mnc=j; }
+    for(int i=0;i<M;i++)
+        for(int j=0;j<N;j++){
+        if(A[i][j]>mx){ 
+            mx=A[i][j]; 
+            mxr=i; 
+            mxc=j;
+        }
+        if(A[i][j]<mn){
+            mn=A[i][j];
+            mnr=i;
+            mnc=j;
+        }
     }
     cout<<"Maior: "<<mx<<" em ("<<mxr<<","<<mxc<<")\nMenor: "<<mn<<" em ("<<mnr<<","<<mnc<<")\n";
 }
-
