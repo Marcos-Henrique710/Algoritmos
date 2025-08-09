d8c42a18355056f58ac567b61a67b988d449d52d
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:43:17 -0300

    Update L5_Q12.cpp

--- Listas de algoritmos/lista 5/L5_Q12.cpp
+++ Listas de algoritmos/lista 5/L5_Q12.cpp
@@ -2,13 +2,22 @@
 12. Leia MxN e escreva o maior elemento da linha que contém o menor elemento da matriz
 */
 void q12(){
-    int M,N; cout<<"Q12: linhas m e colunas n: "; cin>>M>>N;
+    int M,N; 
+    cout<<"Digite linhas m e colunas n: "; 
+    cin>>M>>N;
     vector<vector<int>> A(M, vector<int>(N));
-    for(int i=0;i<M;i++) for(int j=0;j<N;j++) cin>>A[i][j];
+    for(int i=0;i<M;i++) 
+        for(int j=0;j<N;j++) 
+            cin>>A[i][j];
     int mn=A[0][0], mnrow=0;
-    for(int i=0;i<M;i++) for(int j=0;j<N;j++) if(A[i][j]<mn){ mn=A[i][j]; mnrow=i; }
+    for(int i=0;i<M;i++) 
+        for(int j=0;j<N;j++) 
+            if(A[i][j]<mn){
+                mn=A[i][j];
+                mnrow=i;
+            }
     int mx = A[mnrow][0];
-    for(int j=1;j<N;j++) mx = max(mx, A[mnrow][j]);
+    for(int j=1;j<N;j++) 
+        mx = max(mx, A[mnrow][j]);
     cout<<"Menor elemento da matriz: "<<mn<<" na linha "<<mnrow<<"\nMaior elemento dessa linha: "<<mx<<"\n";
 }
-
