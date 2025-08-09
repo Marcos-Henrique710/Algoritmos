8aaa9149a8c1564febab2dbe1baa79f0967f11d2
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:53:32 -0300

    Update L5_Q20.cpp

--- Listas de algoritmos/lista 5/L5_Q20.cpp
+++ Listas de algoritmos/lista 5/L5_Q20.cpp
@@ -3,10 +3,17 @@
 */
 void q20(){
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    string s; cout<<"Q20: digite o nome completo:\n"; getline(cin,s);
-    stringstream ss(s); vector<string> parts; string w;
+    string s; 
+    cout<<"Q20: digite o nome completo:\n"; 
+    getline(cin,s);
+    stringstream ss(s); 
+    vector<string> parts; 
+    string w;
     while(ss>>w) parts.push_back(w);
-    if(parts.empty()) { cout<<"Nome vazio\n"; return; }
+    if(parts.empty()){
+        cout<<"Nome vazio\n";
+        return; 
+    }
     cout<<parts.back()<<", "<<parts.front()<<"\n";
 }
 
