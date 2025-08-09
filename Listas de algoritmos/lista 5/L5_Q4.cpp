28f66512f90297452c75bead95a514779173476e
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:18:39 -0300

    Update L5_Q4.cpp

--- Listas de algoritmos/lista 5/L5_Q4.cpp
+++ Listas de algoritmos/lista 5/L5_Q4.cpp
@@ -4,11 +4,18 @@
 void q4(){
     const int R=15,C=5;
     vector<int> vals;
-    cout<<"Q4: leia 75 inteiros (15x5):\n";
-    for(int i=0;i<R;i++) for(int j=0;j<C;j++){ int x; cin>>x; vals.push_back(x); }
+    cout<<"\n\tdigite 75 inteiros (15x5):\n";
+    for(int i=0;i<R;i++)
+        for(int j=0;j<C;j++){
+            int x; 
+            cin>>x; 
+            vals.push_back(x);
+        }
     map<int,int> freq;
-    for(auto v:vals) freq[v]++;
+    for(auto v:vals) 
+        freq[v]++;
     cout<<"Repetidos:\n";
-    for(auto &p:freq) if(p.second>1) cout<<p.first<<" aparece "<<p.second<<" vezes\n";
+    for(auto &p:freq)
+        if(p.second>1)
+            cout<<p.first<<" aparece "<<p.second<<" vezes\n";
 }
-
