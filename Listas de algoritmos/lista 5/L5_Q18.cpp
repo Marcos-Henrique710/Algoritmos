953ba3ce9cb0f6d613c8399c295e84076145fc95
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:51:45 -0300

    Update L5_Q18.cpp

--- Listas de algoritmos/lista 5/L5_Q18.cpp
+++ Listas de algoritmos/lista 5/L5_Q18.cpp
@@ -3,8 +3,11 @@
 */
 void q18(){
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    string A,B; cout<<"Q18: digite A:\n"; getline(cin,A); cout<<"Digite B:\n"; getline(cin,B);
+    string A,B; 
+    cout<<"Q18: digite A:\n"; 
+    getline(cin,A);
+    cout<<"Digite B:\n"; 
+    getline(cin,B);
     string C = A + (A.size()? " " : "") + B;
     cout<<"C: "<<C<<"\n";
 }
-
