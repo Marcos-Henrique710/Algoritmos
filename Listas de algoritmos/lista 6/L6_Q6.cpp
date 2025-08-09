984a39cbc89af30c9dc74028ddcc427c81561666
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 22:16:39 -0300

    Update L6_Q6.cpp

--- Listas de algoritmos/lista 6/L6_Q6.cpp
+++ Listas de algoritmos/lista 6/L6_Q6.cpp
@@ -1,12 +1,20 @@
 /*
 6. 200 funcionarios: nome, horas, dependentes; calcular salarios e descontos
 */
-struct Func{ string nome; int horas; int dependentes; };
+struct Func{ 
+string nome;
+int horas; 
+int dependentes; 
+};
 void q6(){
     vector<Func> v; v.reserve(200);
     cout<<"Cadastrar funcionarios (digite 'fim' como nome para terminar):\n";
     while(v.size()<200){
-        Func f; cin>>f.nome; if(f.nome=="fim") break; cin>>f.horas>>f.dependentes; v.push_back(f);
+        Func f; 
+        cin>>f.nome; 
+        if(f.nome=="fim") 
+            break;
+        cin>>f.horas>>f.dependentes; v.push_back(f);
     }
     for(auto &f:v){
         double bruto = 12.0*f.horas + 40.0*f.dependentes;
@@ -15,4 +23,3 @@ void q6(){
         cout<<f.nome<<" Salario bruto: "<<bruto<<" INSS: "<<inss<<" IR: "<<ir<<" Salario liquido: "<<liquido<<"\n";
     }
 }
-
