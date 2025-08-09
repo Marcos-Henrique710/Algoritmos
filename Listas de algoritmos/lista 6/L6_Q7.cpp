da1903b7ae5911bc8f1d54edb30c8d96851d1bdb
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 22:17:19 -0300

    Update L6_Q7.cpp

--- Listas de algoritmos/lista 6/L6_Q7.cpp
+++ Listas de algoritmos/lista 6/L6_Q7.cpp
@@ -1,6 +1,17 @@
 /*
 7. Registro cliente banco e menu (15 clientes): cadastrar, imprimir por CPF, imprimir negativos
 */
-struct Endereco{ string rua; int numero; string bairro; string cidade; string estado; };
-struct Pessoa{ string nome; Endereco ender; string telefone; int idade; };
+struct Endereco{
+string rua;
+int numero; 
+string bairro;
+string cidade; 
+string estado;
+};
+struct Pessoa{
+string nome; 
+Endereco ender;
+string telefone;
+int idade; 
+};
 void q9_helper(){ 
