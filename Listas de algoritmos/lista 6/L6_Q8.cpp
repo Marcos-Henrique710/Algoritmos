1c70faa24ea636bdb5dbd2d75b4265560103dd2c
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 22:17:51 -0300

    Update L6_Q8.cpp

--- Listas de algoritmos/lista 6/L6_Q8.cpp
+++ Listas de algoritmos/lista 6/L6_Q8.cpp
@@ -1,4 +1,1 @@
-/* placeholder for question 9 of Lista6 which is different; we'll implement q7 to q... as per lista */ }
-
-// Implementacoes restantes conforme enunciados (reduzidas/completas suficientes)
 
