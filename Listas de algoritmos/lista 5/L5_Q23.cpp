d8721c954b06fbc25c0f439bcc8231592537e794
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:56:57 -0300

    Update L5_Q23.cpp

--- Listas de algoritmos/lista 5/L5_Q23.cpp
+++ Listas de algoritmos/lista 5/L5_Q23.cpp
@@ -3,7 +3,20 @@
 */
 void q23(){
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    string a,b; cout<<"Q23: digite string A:\n"; getline(cin,a); cout<<"Digite string B:\n"; getline(cin,b);
-    if(a.size()!=b.size()) cout<<"Diferentes\n"; else{ bool eq=true; for(size_t i=0;i<a.size();i++) if(a[i]!=b[i]){ eq=false; break; } cout<<(eq?"Iguais\n":"Diferentes\n"); }
+    string a,b; 
+    cout<<"Q23: digite string A:\n";
+    getline(cin,a); 
+    cout<<"Digite string B:\n"; 
+    getline(cin,b);
+    if(a.size()!=b.size())
+        cout<<"Diferentes\n"; 
+    else{ 
+        bool eq=true; 
+        for(size_t i=0;i<a.size();i++)
+            if(a[i]!=b[i]){ 
+                eq=false;
+                break;
+            } 
+        cout<<(eq?"Iguais\n":"Diferentes\n");
+    }
 }
-
