c45f80a3fcb2951ceb08f0553e2a714c7e96044b
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:48:09 -0300

    Update L5_Q14.cpp

--- Listas de algoritmos/lista 5/L5_Q14.cpp
+++ Listas de algoritmos/lista 5/L5_Q14.cpp
@@ -3,11 +3,20 @@
 */
 void q14(){
     const int R=4,C=5;
-    double M[R][C]; cout<<"Q14: leia 20 reais (4x5):\n";
-    for(int i=0;i<R;i++) for(int j=0;j<C;j++) cin>>M[i][j];
+    double M[R][C]; 
+    cout<<"Q14: leia 20 reais (4x5):\n";
+    for(int i=0;i<R;i++)
+        for(int j=0;j<C;j++)
+            cin>>M[i][j];
     double S[C] = {0};
-    for(int j=0;j<C;j++) for(int i=0;i<R;i++) S[j]+=M[i][j];
-    cout<<"Matriz:\n"; for(int i=0;i<R;i++){ for(int j=0;j<C;j++) cout<<M[i][j]<<" "; cout<<"\n"; }
+    for(int j=0;j<C;j++)
+        for(int i=0;i<R;i++)
+            S[j]+=M[i][j];
+    cout<<"Matriz:\n"; 
+    for(int i=0;i<R;i++){ 
+        for(int j=0;j<C;j++) 
+            cout<<M[i][j]<<" ";
+        cout<<"\n";
+    }
     cout<<"Vetor soma por coluna: "; for(int j=0;j<C;j++) cout<<S[j]<<" "; cout<<"\n";
 }
-
