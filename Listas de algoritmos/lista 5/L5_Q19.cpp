7bc924a98a271093c2e1a292a060b60b4b85b7eb
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:52:34 -0300

    Update L5_Q19.cpp

--- Listas de algoritmos/lista 5/L5_Q19.cpp
+++ Listas de algoritmos/lista 5/L5_Q19.cpp
@@ -3,8 +3,11 @@
 */
 void q19(){
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    string s; cout<<"Q19: digite a string:\n"; getline(cin,s);
-    string r = s; reverse(r.begin(), r.end());
+    string s; 
+    cout<<"digite uma string:\n"; 
+    getline(cin,s);
+    string r = s; 
+    reverse(r.begin(), r.end());
     cout<<"Invertida: "<<r<<"\n";
 }
 
