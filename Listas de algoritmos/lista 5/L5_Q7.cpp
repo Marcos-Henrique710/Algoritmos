ec4a7798694aa4dc14c84120ea2882a4d62a3773
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:34:26 -0300

    Update L5_Q7.cpp

--- Listas de algoritmos/lista 5/L5_Q7.cpp
+++ Listas de algoritmos/lista 5/L5_Q7.cpp
@@ -1,12 +1,14 @@
 /*
 7. Gere a matriz específica 6x6
 */
-void q7(){
+int main(){
     int M[6][6];
-    for(int i=0;i<6;i++) for(int j=0;j<6;j++){
-        M[i][j] = min(min(i+1,j+1), min(6-i,6-j));
+    for(int i=0;i<6;i++) 
+        for(int j=0;j<6;j++){
+            M[i][j] = min(min(i+1,j+1), min(6-i,6-j));
     }
     cout<<"Matriz gerada:\n";
-    for(int i=0;i<6;i++){ for(int j=0;j<6;j++) cout<<M[i][j]<<" "; cout<<"\n"; }
+    for(int i=0;i<6;i++){ 
+        for(int j=0;j<6;j++)
+            cout<<M[i][j]<<" "; cout<<"\n"; }
 }
-
