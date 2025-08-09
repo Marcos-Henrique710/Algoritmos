3adf9a7e11e9b64d8b990857c70cebf293d8b82e
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:13:25 -0300

    Update Q27_Lista_4.cpp

--- Listas de algoritmos/lista 4/Q27_Lista_4.cpp
+++ Listas de algoritmos/lista 4/Q27_Lista_4.cpp
@@ -11,8 +11,11 @@ Elabore um algoritmo que leia um vetor de 10 posições de inteiros, ordene -o (
 using namespace std;
 
 int main(){
-    vector<int> v(10); for(int i=0;i<10;i++) cin>>v[i];
+    vector<int> v(10); 
+    for(int i=0;i<10;i++) 
+        cin>>v[i];
     sort(v.begin(), v.end());
-    for(int i=0;i<10;i++) cout<<v[i]<<(i==9?'\n':' ');
+    for(int i=0;i<10;i++) 
+        cout<<v[i]<<(i==9?'\n':' ');
     return 0;
 }
