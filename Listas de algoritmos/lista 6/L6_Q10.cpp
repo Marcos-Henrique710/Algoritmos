fecc11511f287808a0c9b7633cd3fcf798c7e103
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 22:22:31 -0300

    Update L6_Q10.cpp

--- Listas de algoritmos/lista 6/L6_Q10.cpp
+++ Listas de algoritmos/lista 6/L6_Q10.cpp
@@ -1,13 +1,28 @@
 /*
 8. Provedor de internet cadastro clientes e calculo valor a pagar
 */
-struct ClienteProv{ int codigo; string email; int horas; char pagina; };
+struct ClienteProv{ 
+int codigo;
+string email;
+int horas; 
+char pagina;
+};
 void q8(){
-    vector<ClienteProv> v; cout<<"Insira clientes (codigo -1 para terminar):\n";
-    while(v.size()<500){ ClienteProv c; cin>>c.codigo; if(c.codigo==-1) break; cin>>c.email>>c.horas>>c.pagina; v.push_back(c); }
+    vector<ClienteProv> v; 
+    cout<<"Insira clientes (codigo -1 para terminar):\n";
+    while(v.size()<500){ 
+        ClienteProv c;
+        cin>>c.codigo; 
+        if(c.codigo==-1) 
+            break; 
+        cin>>c.email>>c.horas>>c.pagina; v.push_back(c);
+    }
     for(auto &c:v){
-        double val = 35.0; int extra = max(0, c.horas-20); val += extra*2.5; if(toupper(c.pagina)=='S') val+=40.0;
+        double val = 35.0;
+        int extra = max(0, c.horas-20);
+        val += extra*2.5;
+        if(toupper(c.pagina)=='S') 
+            val+=40.0;
         cout<<"Codigo "<<c.codigo<<" valor a pagar: "<<val<<"\n";
     }
 }
-
