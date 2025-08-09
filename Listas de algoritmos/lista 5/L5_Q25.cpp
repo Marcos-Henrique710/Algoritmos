91e0bee5989ce9c8d37ee1a27a707f33319c6c31
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:58:42 -0300

    Update L5_Q25.cpp

--- Listas de algoritmos/lista 5/L5_Q25.cpp
+++ Listas de algoritmos/lista 5/L5_Q25.cpp
@@ -3,9 +3,14 @@
 */
 void q25(){
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    string s; cout<<"Q25: digite a string:\n"; getline(cin,s);
-    string filt; for(char c: s) if(isalnum((unsigned char)c)) filt+=tolower(c);
-    string r=filt; reverse(r.begin(), r.end());
+    string s; 
+    cout<<"Q25: digite a string:\n"; getline(cin,s);
+    string filt; 
+    for(char c: s)
+        if(isalnum((unsigned char)c))
+            filt+=tolower(c);
+    string r=filt;
+    reverse(r.begin(), r.end());
     cout<<(r==filt?"Eh palindromo\n":"Nao eh palindromo\n");
 }
 
