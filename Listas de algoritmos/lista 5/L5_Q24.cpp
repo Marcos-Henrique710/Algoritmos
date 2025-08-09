3fac695b2370d565afac8841c5b49df975f9d9ac
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:57:50 -0300

    Update L5_Q24.cpp

--- Listas de algoritmos/lista 5/L5_Q24.cpp
+++ Listas de algoritmos/lista 5/L5_Q24.cpp
@@ -3,8 +3,13 @@
 */
 void q24(){
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    int n; cout<<"Q24: digite n (shift): "; cin>>n; cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    string s; cout<<"Digite a string:\n"; getline(cin,s);
+    int n;
+    cout<<"Q24: digite n (shift): "; 
+    cin>>n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    string s; 
+    cout<<"Digite a string:\n"; 
+    getline(cin,s);
     for(char &c: s){
         if(isalpha((unsigned char)c)){
             char base = isupper((unsigned char)c)?'A':'a';
@@ -13,4 +18,3 @@ void q24(){
     }
     cout<<"Codificada: "<<s<<"\n";
 }
-
