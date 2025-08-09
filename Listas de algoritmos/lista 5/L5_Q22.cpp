b42e8adc2ce509a2b73c04ec06413ca76459f7bd
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 21:55:55 -0300

    Update L5_Q22.cpp

--- Listas de algoritmos/lista 5/L5_Q22.cpp
+++ Listas de algoritmos/lista 5/L5_Q22.cpp
@@ -3,10 +3,20 @@
 */
 void q22(){
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    string s; cout<<"Q22: digite a frase:\n"; getline(cin,s);
+    string s; cout<<"digite uma frase:\n"; 
+    getline(cin,s);
     cout<<"Frase completa: "<<s<<"\n";
-    stringstream ss(s); string w; int idx=1; string out;
-    while(ss>>w){ if(idx%2==1){ if(!out.empty()) out+=" "; out+=w; } idx++; }
+    stringstream ss(s); 
+    string w; 
+    int idx=1;
+    string out;
+    while(ss>>w){ 
+        if(idx%2==1){ 
+            if(!out.empty()) 
+                out+=" "; 
+            out+=w; 
+        } 
+        idx++;
+    }
     cout<<"Palavras em posicoes impares: "<<out<<"\n";
 }
-
