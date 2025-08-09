132920b4e90117a3967e9a456f4cfe0bab9ba038
Marcos Henrique <marcoshenriqueufrn@gmail.com>
2025-08-08 22:12:12 -0300

    Update L6_Q4.cpp

--- Listas de algoritmos/lista 6/L6_Q4.cpp
+++ Listas de algoritmos/lista 6/L6_Q4.cpp
@@ -4,10 +4,19 @@
 void q4(){
     vector<Cliente> v; v.reserve(20);
     while(v.size()<20){
-        cout<<"Cadastrar cliente? (S/N) "; char c; cin>>c; if(toupper(c)=='N') break;
-        Cliente cl; cout<<"Identidade Nome Endereco Telefone\n"; cin>>cl.identidade; cin.ignore(); getline(cin,cl.nome); getline(cin,cl.endereco); getline(cin,cl.telefone);
+        cout<<"Cadastrar cliente? (S/N) "; 
+        char c;
+        cin>>c; 
+        if(toupper(c)=='N')
+            break;
+        Cliente cl;
+        cout<<"Identidade Nome Endereco Telefone\n"; 
+        cin>>cl.identidade;
+        cin.ignore(); 
+        getline(cin,cl.nome);
+        getline(cin,cl.endereco); 
+        getline(cin,cl.telefone);
         v.push_back(cl);
     }
     cout<<"Relatorio:\n"; for(auto &c:v) cout<<c.identidade<<" "<<c.nome<<" "<<c.endereco<<" "<<c.telefone<<"\n";
 }
-
